40c34288f6eec99de6d498fad736f0633e7bc6d1
Omar Velasco <ovelasco@magnabyte.com.mx>
2020-12-21 14:24:37 -0600

    Prueba de comando jar y uso en parte del classpath para single-file source-code

---
+++ chapter1/compiling-topic/OtroTest.java
@@ -0,0 +1,5 @@
+public class OtroTest {
+    public static void main(String[] args) {
+        System.out.println("El otro test");
+    }
+}
---
+++ chapter1/compiling-topic/sub/TestEnSub.java
@@ -0,0 +1,5 @@
+public class TestEnSub {
+    public static void main(String[] args) {
+        System.out.println("Test de clase en subdirectorio");
+    }
+}
